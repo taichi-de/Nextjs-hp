fc4df4a27961322e339c393d081eef8e5d17cd17
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-03-14 07:15:13 +0100

    add blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import Link from "next/link";
@@ -17,21 +18,23 @@ const Page: NextPage<PageProps> = (props) => {
   const { siteData, blogList } = props;
 
   return (
-    <main>
-      <h1>{siteData.title}</h1>
-      <section>
-        <h2 className="text-4xl text-red-700">ブログ一覧</h2>
-        <ul>
-          {blogList.contents.map((blog) => (
-            <li key={blog.id}>
-              <Link href={`/blogs/${blog.id}`}>
-                <a className="text-lg text-gray-400">{blog.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </main>
+    <Layout title="About">
+      <div className="p-8 overflow-y-scroll leading-loose text-center text-white md:w-4/12 overscroll-y-auto h-4/5 sm:w-full">
+        <h1>{siteData.title}</h1>
+        <section>
+          <h2 className="text-4xl text-red-700">ブログ一覧</h2>
+          <ul>
+            {blogList.contents.map((blog) => (
+              <li key={blog.id}>
+                <Link href={`/blogs/${blog.id}`}>
+                  <a className="text-lg text-gray-400">{blog.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
+    </Layout>
   );
 };
 
