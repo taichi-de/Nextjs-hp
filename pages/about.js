4664b03450943cd052afbd76594123bc0cb5ad30
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-04-05 15:15:14 +0200

    error with '@/path'

--- pages/about.js
+++ pages/about.js
@@ -1,49 +1,57 @@
-import Layout from "../components/Layout";
-import Image from "next/image";
+import siteMetadata from '@/data/siteMetadata'
+import SocialIcon from '@/components/social-icons'
+import { PageSeo } from '@/components/SEO'
 
 export default function About() {
   return (
-    <Layout title="About">
-      <div className="p-8 overflow-y-scroll leading-loose text-center text-white md:w-4/12 overscroll-y-auto h-4/5 sm:w-full">
-        <div className="mb-4">
-          <p className="mb-4 font-mono text-lg text-bold">About me</p>
+    <>
+      <PageSeo
+        title={`About - ${siteMetadata.author}`}
+        description={`About me - ${siteMetadata.author}`}
+        url={`${siteMetadata.siteUrl}/about`}
+      />
+      <div className="divide-y">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            About
+          </h1>
         </div>
-        <div className="flex justify-center m-4">
-          <Image
-            className="rounded-full"
-            src="/profile.jpeg"
-            width={120}
-            height={120}
-            alt="profile"
-          />
-        </div>
-        <div>
-          <h4>Hello! I am Taichi Tomioka</h4>
-          <p>
-            I'm from Japan and studied Agriculture in Akita(Northern Japan).
-            <br />
-            After graduating from university, I moved to Germany. <br />
-            Currently working as a Working student at a german web company while
-            majoring in computer science at a university.
-          </p>
-          <p className="my-3">
-            Skills：
-            <br />
-            HTML / CSS(Sass) / JavaScript(jQuery, React, TS, Nexjs) /
-            Ruby(Rails) / GraphQL / Slim / Wordpress
-          </p>
-          <p className="my-3">
-            Languages：
-            <br />
-            Japanese + English / German
-          </p>
-          <p className="my-3">
-            Love：
-            <br />
-            Movies & Coffee & Workout
-          </p>
+        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+          <div className="flex flex-col items-center pt-8 space-x-2">
+            <img src={siteMetadata.image} alt="avatar" className="w-48 h-48 rounded-full" />
+            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
+              {siteMetadata.author}
+            </h3>
+            <div className="text-gray-500 dark:text-gray-400">Professor of Atmospheric Science</div>
+            <div className="text-gray-500 dark:text-gray-400">Stanford University</div>
+            <div className="flex pt-6 space-x-3">
+              <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
+              <SocialIcon kind="github" href={siteMetadata.github} />
+              <SocialIcon kind="facebook" href={siteMetadata.facebook} />
+              <SocialIcon kind="youtube" href={siteMetadata.youtube} />
+              <SocialIcon kind="linkedin" href={siteMetadata.linkedin} />
+              <SocialIcon kind="twitter" href={siteMetadata.twitter} />
+            </div>
+          </div>
+          <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
+            <p>
+              Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His
+              research interests includes complexity modelling of tailwinds, headwinds and
+              crosswinds.
+            </p>
+            <p>
+              He leads the clean energy group which develops 3D air pollution-climate models, writes
+              differential equation solvers, and manufactures titanium plated air ballons. In his
+              free time he bakes raspberry pi.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique
+              placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem
+              nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
+            </p>
+          </div>
         </div>
       </div>
-    </Layout>
-  );
+    </>
+  )
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,41 +1,23 @@
-import Layout from "../components/Layout";
-import Link from "next/link";
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+import siteMetadata from '@/data/siteMetadata'
+import ListLayout from '@/layouts/ListLayout'
+import { PageSeo } from '@/components/SEO'
 
-export default function Blog({ blog }) {
-  return (
-    <Layout title="Blog">
-      <div className="flex-col justify-center w-3/5 p-6 m-6 text-center bg-gray-200 shadow-xl rounded-xl">
-        <div className="mb-4">
-          <p className="font-bold">Achieve</p>
-        </div>
-        <div className="text-left pl-80">
-          <ul>
-            {blog.map((blog) => (
-              <li key={blog.id}>
-                <Link href={`blog/${blog.id}`}>
-                  <a className="py-4 my-2 border-b border-gray-300 font-base">
-                    {blog.title}
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </Layout>
-  );
+export async function getStaticProps() {
+  const posts = await getAllFilesFrontMatter('blog')
+
+  return { props: { posts } }
 }
 
-export const getStaticProps = async () => {
-  const key = {
-    headers: { "X-API-KEY": process.env.API_KEY },
-  };
-  const data = await fetch("https://taizen-dev.microcms.io/api/v1/blog", key)
-    .then((res) => res.json())
-    .catch(() => null);
-  return {
-    props: {
-      blog: data.contents,
-    },
-  };
-};
+export default function Blog({ posts }) {
+  return (
+    <>
+      <PageSeo
+        title={`Blog - ${siteMetadata.author}`}
+        description={siteMetadata.description}
+        url={`${siteMetadata.siteUrl}/blog`}
+      />
+      <ListLayout posts={posts} title="All Posts" />
+    </>
+  )
+}
